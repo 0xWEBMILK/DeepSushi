a616b1a4b511b9fd32085f21273f78177ef541ae
Milk Web <0xwebmilk@gmail.com>
2024-11-20 20:02:28 +0300

    Added promotion page

--- src/frontend/components/PromotionCard/PromotionCard.jsx
+++ src/frontend/components/PromotionCard/PromotionCard.jsx
@@ -1,22 +1,23 @@
 import "./PromotionCard.css";
 
-
-const PromotionCard = (props) => {
-
-    const {img, time, titel, description} = props
+const PromotionCard = ({ img, time, titel, description }) => {
   return (
-    <>
-      <div className="promotion__card">
-        <div>
-          <img src={img} alt="" />
-        </div>
-        <p className="card__time">{time}</p>
-        <a href="#" className="promotion__card__titel">
-          {titel}
-        </a>
-        <p className="promotion__card__description">{description}</p>
+    <div className="promotion__card">
+      <div className="promotion__card__image-wrapper">
+        <img
+          src={img}
+          alt={titel}
+          className="promotion__card__image"
+        />
       </div>
-    </>
+      <p className="promotion__card__time">{time}</p>
+      <a href="#" className="promotion__card__titel">
+        {titel}
+      </a>
+      <p className="promotion__card__description">
+        {description}
+      </p>
+    </div>
   );
 };
 
--- src/frontend/src/pages/PromotionPage/PromotionPage.jsx
+++ src/frontend/src/pages/PromotionPage/PromotionPage.jsx
@@ -1,52 +1,41 @@
+import React, { useEffect, useState } from "react";
 import PromotionCard from "../../../components/PromotionCard/PromotionCard";
-import promotionImgOne from "../../../Images/promotion-1.png";
 import "./PromotionPage.css";
 
 const PromotionPage = () => {
+  const [promotions, setPromotions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/promotion") 
+      .then((response) => {
+        console.log(response);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setPromotions(data);
+      })
+      .catch((error) => console.error("Ошибка загрузки данных:", error));
+  }, []);
+
   return (
-    <>
-      <section className="promotion">
-        <h2 className="promotion__titel">Новости и акции</h2>
-        <div className="promotion__grid">
+    <section className="promotion">
+      <h2 className="promotion__titel">Новости и акции</h2>
+      <div className="promotion__grid">
+        {promotions.map((promotion, index) => (
           <PromotionCard
-            img={promotionImgOne}
-            time="15 МАЯ 2024"
-            titel="-25% на доставку"
-            description="Скидка на первый заказ"
+            key={index}
+            img={promotion.file_path}
+            time={promotion.date}
+            titel={promotion.title}
+            description={promotion.description}
           />
-          <PromotionCard
-            img={promotionImgOne}
-            time="14 ноября 2024"
-            titel="Бенто остров стал другим!"
-            description="Обновленное детское меню"
-          />
-          <PromotionCard
-            img={promotionImgOne}
-            time="12 ноября 2024"
-            titel="Особенное предложение"
-            description="Меню к 25-летию «Якитории»"
-          />
-          <PromotionCard
-            img={promotionImgOne}
-            time="12 ноября 2024"
-            titel="Особенное предложение"
-            description="Меню к 25-летию «Якитории»"
-          />
-          <PromotionCard
-            img={promotionImgOne}
-            time="12 ноября 2024"
-            titel="Особенное предложение"
-            description="Меню к 25-летию «Якитории»"
-          />
-          <PromotionCard
-            img={promotionImgOne}
-            time="12 ноября 2024"
-            titel="Особенное предложение"
-            description="Меню к 25-летию «Якитории»"
-          />
-        </div>
-      </section>
-    </>
+        ))}
+      </div>
+    </section>
   );
 };
 
