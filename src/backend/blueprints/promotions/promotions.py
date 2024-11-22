0f18fcc5148f98e8987eec04911ef5cafb1f5dd6
Milk Web <0xwebmilk@gmail.com>
2024-11-22 09:05:37 +0300

    Some changes

---
+++ src/backend/blueprints/promotions/promotions.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, jsonify
+from models.models import Promotion
+
+promotions_blueprint = Blueprint('promotions_blueprint', __name__)
+
+@promotions_blueprint.route('/promotions', methods=['GET'])
+def index():
+    list_of_subject = Promotion.query.all()
+    subject_list = [subject.to_dict() for subject in list_of_subject]
+    return jsonify(subject_list)
