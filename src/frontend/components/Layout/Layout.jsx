72e4e1774b03c6bb20ca10be3251a9a53cdab30e
ArtyomBelavin <artyombelavin@gmail.com>
2024-11-24 18:51:22 +0500

    Frontend some changes

--- src/frontend/components/Layout/Layout.jsx
+++ src/frontend/components/Layout/Layout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import SideBar from "../SideBar/SideBar";
-
 import "./Layout.css";
 import Present from "../Main/Present/Present";
 
--- src/frontend/components/Main/Main.jsx
+++ src/frontend/components/Main/Main.jsx
@@ -1,5 +1,4 @@
 import Background from "./Background/Background";
-import Present from "./Present/Present";
 
 import "./Main.css";
 
@@ -7,7 +6,6 @@ const Main = () => {
   return (
     <>
       <Background title={"25 лет держим палочки!"} description={"Давайте отмечать вместе!"} />
-      <Present />
     </>
   );
 };
