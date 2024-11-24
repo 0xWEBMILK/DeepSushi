70695f4d02901f3bb0613f65d046a6e0f81ba67c
D. Adamson <0xwebmilk@gmail.com>
2024-11-25 01:58:15 +0300

    Cities menu added

--- src/backend/blueprints/promotions/promotions.py
+++ src/backend/blueprints/promotions/promotions.py
@@ -4,7 +4,7 @@ from models.models import Promotion
 promotions_blueprint = Blueprint('promotions_blueprint', __name__)
 
 @promotions_blueprint.route('/promotions', methods=['GET'])
-def index():
+def get_promotions():
     list_of_subject = Promotion.query.all()
     subject_list = [subject.to_dict() for subject in list_of_subject]
     return jsonify(subject_list)
